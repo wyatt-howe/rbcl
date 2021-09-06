a2247cea95536999ca6b53dbaf2b96bde171ceb1
Wyatt Howe <wyatt_howe@brown.edu>
2021-09-05 21:30:23 -0400

    Increment version to 0.1.3

--- setup.py
+++ setup.py
@@ -217,7 +217,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="rbcl",
-    version="0.1.2",
+    version="0.1.3",
     license="Apache",
     url="https://github.com/nthparty/rbcl",
     author="Wyatt Howe",
