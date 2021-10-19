69d0cfde5fa3e7049fdd65247a12be5b90b8d824
Wyatt Howe <43802211+wyatt-howe@users.noreply.github.com>
2021-10-19 11:27:39 -0400

    Add `cffi` and `six` as dependencies

--- setup.py
+++ setup.py
@@ -228,6 +228,7 @@ setup(
     long_description_content_type="text/x-rst",
     test_suite="nose.collector",
     tests_require=["nose"],
+    install_requires=['cffi', 'six'],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     package_dir={"": "rbcl"},
     packages=["rbcl", "rbcl.bindings"],
