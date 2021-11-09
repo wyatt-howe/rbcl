e96c712a1dfa28c2f1dbdf407e01055df4dfa543
Wyatt Howe <wyatt_howe@brown.edu>
2021-11-09 11:19:22 -0500

    Merge setup.py from nthparty/bcl

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@
 Setup, package, and build file for the rbcl cryptography library.
 """
 import sys
+import platform
 import os
 import os.path
-import platform
+import shutil
 import glob
 import subprocess
+import tarfile
 import errno
-import functools
+import urllib.request
 from distutils.sysconfig import get_config_vars
 from setuptools import Distribution, setup
 from setuptools.command.build_ext import build_ext as _build_ext
@@ -18,74 +20,108 @@ try:
 except ImportError:
     from distutils.command.build_clib import build_clib as _build_clib
 
-def here(*paths):
-    return os.path.relpath(os.path.join(*paths))
+def prepare_libsodium_source_tree(libsodium_folder='rbcl/libsodium'):
+    """
+    Retrieve the libsodium source archive and extract it
+    to the location used by the build process.
+    """
+    # URL from which libsodium source archive is retrieved,
+    # and paths into which it is extracted and then moved.
+    url = (
+        'https://github.com/jedisct1/libsodium/releases' + \
+        '/download/1.0.18-RELEASE/libsodium-1.0.18.tar.gz'
+    )
+    libsodium_tar_gz_path = './rbcl/libsodium.tar.gz'
+    libsodium_tar_gz_folder = './rbcl/libsodium_tar_gz'
 
-def abshere(*paths):
-    return os.path.abspath(here(*paths))
+    # Download the source archive to a local path (unless
+    # it is already present).
+    if not os.path.exists(libsodium_tar_gz_path):
+        try:
+            urllib.request.urlretrieve(url, filename=libsodium_tar_gz_path)
+        except:
+            raise RuntimeError(
+                'failed to download libsodium archive and no local ' + \
+                'archive was found at `' + libsodium_tar_gz_path + '`'
+            ) from None
 
-sodium = functools.partial(here, "rbcl/libsodium/")
+    # Extract the archive into a temporary folder (removing
+    # the folder if it already exists).
+    with tarfile.open(libsodium_tar_gz_path) as libsodium_tar_gz:
+        if os.path.exists(libsodium_tar_gz_folder):
+            shutil.rmtree(libsodium_tar_gz_folder)
+        libsodium_tar_gz.extractall(libsodium_tar_gz_folder)
 
-def which(name, flags=os.X_OK): # Taken from twisted.
-    result = []
-    exts = filter(None, os.environ.get("PATHEXT", "").split(os.pathsep))
-    path = os.environ.get("PATH", None)
-    if path is None:
-        return []
-    for p in os.environ.get("PATH", "").split(os.pathsep):
-        p = os.path.join(p, name)
-        if os.access(p, flags):
-            result.append(p)
-        for e in exts:
-            pext = p + e
-            if os.access(pext, flags):
-                result.append(pext)
-    return result
+    # Move the source tree to the destination folder (removing
+    # the destination folder first, if it already exists).
+    if os.path.exists(libsodium_folder):
+        shutil.rmtree(libsodium_folder)
+    shutil.move(
+        libsodium_tar_gz_folder + '/libsodium-1.0.18',
+        libsodium_folder
+    )
 
-def use_system():
-    if os.environ.get("SODIUM_INSTALL") == "system":
-        return True # Don't compile the bundled sodium copy.
-    else:
-        return False # Use the bundled copy.
+    # Remove the archive and temporary folder.
+    os.remove(libsodium_tar_gz_path)
+    shutil.rmtree(libsodium_tar_gz_folder)
+
+    return libsodium_folder
 
 class Distribution(Distribution):
     def has_c_libraries(self):
-        return not use_system()
+        # On Windows, only a precompiled dynamic library file is used.
+        return not sys.platform == 'win32'
 
 class build_clib(_build_clib):
     def get_source_files(self):
-        files = glob.glob(here("rbcl/libsodium/*"))
-        files += glob.glob(here("rbcl/libsodium/*/*"))
-        files += glob.glob(here("rbcl/libsodium/*/*/*"))
-        files += glob.glob(here("rbcl/libsodium/*/*/*/*"))
-        files += glob.glob(here("rbcl/libsodium/*/*/*/*/*"))
-        files += glob.glob(here("rbcl/libsodium/*/*/*/*/*/*"))
-        files += glob.glob(here("rbcl/libsodium/*/*/*/*/*/*/*"))
-        return files
+        return [
+            file
+            for i in range(1, 8)
+            for file in glob.glob(os.path.relpath('rbcl/libsodium' + ('/*' * i)))
+        ]
 
     def build_libraries(self, libraries):
-        raise Exception("build_libraries")
+        raise RuntimeError('`build_libraries` should not be invoked')
 
     def check_library_list(self, libraries):
-        raise Exception("check_library_list")
+        raise RuntimeError('`check_library_list` should not be invoked')
 
     def get_library_names(self):
-        return ["sodium"]
+        return ['sodium']
 
     def run(self):
-        if use_system():
+        # On Windows, only a precompiled dynamic library file is used.
+        if sys.platform == 'win32':
             return
 
-        # Use Python's build environment variables.
-        build_env = {
-            key: val
-            for key, val in get_config_vars().items()
-            if key in ("LDFLAGS", "CFLAGS", "CC", "CCSHARED", "LDSHARED")
-            and key not in os.environ
-        }
-        os.environ.update(build_env)
+        # Confirm that make utility can be found.
+        found = False
+        if not os.environ.get('PATH', None) is None:
+            for p in os.environ.get('PATH', '').split(os.pathsep):
+                p = os.path.join(p, 'make')
+                if os.access(p, os.X_OK):
+                    found = True
+                for e in filter(
+                    None,
+                    os.environ.get('PATHEXT', '').split(os.pathsep)
+                ):
+                    if os.access(p + e, os.X_OK):
+                        found = True
+        if not found:
+            raise RuntimeError('make utility cannot be found')
 
-        # Ensure our temporary build directory exists.
+        # Reproduce Python's build environment variables.
+        os.environ.update({
+            variable: value
+            for (variable, value) in get_config_vars().items()
+            if (
+                variable in [
+                    'LDFLAGS', 'CFLAGS', 'CC', 'CCSHARED', 'LDSHARED'
+                ] and variable not in os.environ
+            )
+        })
+
+        # Ensure the temporary build directory exists.
         build_temp = os.path.abspath(self.build_temp)
         try:
             os.makedirs(build_temp)
@@ -93,63 +129,40 @@ class build_clib(_build_clib):
             if e.errno != errno.EEXIST:
                 raise
 
-        # Ensure all of our executable files have their permissions set.
+        # Retrieve (if necessary) and extract the libsodium source tree.
+        libsodium_folder = prepare_libsodium_source_tree()
+
+        # Ensure that all executable files have the necessary permissions.
         for filename in [
-            "rbcl/libsodium/autogen.sh",
-            "rbcl/libsodium/compile",
-            "rbcl/libsodium/configure",
-            "rbcl/libsodium/depcomp",
-            "rbcl/libsodium/install-sh",
-            "rbcl/libsodium/missing",
-            "rbcl/libsodium/msvc-scripts/process.bat",
-            "rbcl/libsodium/test/default/wintest.bat",
+            'autogen.sh', 'compile', 'configure', 'depcomp', 'install-sh',
+            'missing', 'msvc-scripts/process.bat', 'test/default/wintest.bat',
         ]:
-            os.chmod(here(filename), 0o755)
+            os.chmod(os.path.relpath(libsodium_folder + '/' + filename), 0o755)
 
-        if not which("make"):
-            raise Exception("ERROR: The 'make' utility is missing from PATH")
-
-        # Locate our configure script.
-        configure = abshere("rbcl/libsodium/configure")
-
-        # Run `./configure`.
-        configure_flags = [
-            "--disable-shared",
-            "--enable-static",
-            "--disable-debug",
-            "--disable-dependency-tracking",
-            "--with-pic",
-        ]
-        if platform.system() == "SunOS":
-            # On Solaris, libssp doesn't link statically and causes linker
-            # errors during import.
-            configure_flags.append("--disable-ssp")
-        if os.environ.get("SODIUM_INSTALL_MINIMAL"):
-            configure_flags.append("--enable-minimal")
+        # Configure libsodium, build it as a shared library file, check it,
+        # and install it.
         subprocess.check_call(
-            [configure]
-            + configure_flags
-            + ["--prefix", os.path.abspath(self.build_clib)],
-            cwd=build_temp,
+            [os.path.abspath(os.path.relpath('rbcl/libsodium/configure'))] + \
+            [
+                '--disable-shared', '--enable-static',
+                '--disable-debug', '--disable-dependency-tracking', '--with-pic',
+            ] + \
+            (['--disable-ssp'] if platform.system() == 'SunOS' else []) + \
+            ['--prefix', os.path.abspath(self.build_clib)],
+            cwd=build_temp
         )
-
-        make_args = os.environ.get("LIBSODIUM_MAKE_ARGS", "").split()
-        # Build the library.
-        subprocess.check_call(["make"] + make_args, cwd=build_temp)
-
-        # Check the built library.
-        subprocess.check_call(["make", "check"] + make_args, cwd=build_temp)
-
-        # Install the built library.
-        subprocess.check_call(["make", "install"] + make_args, cwd=build_temp)
+        make_args = os.environ.get('LIBSODIUM_MAKE_ARGS', '').split()
+        subprocess.check_call(['make'] + make_args, cwd=build_temp)
+        subprocess.check_call(['make', 'check'] + make_args, cwd=build_temp)
+        subprocess.check_call(['make', 'install'] + make_args, cwd=build_temp)
 
 class build_ext(_build_ext):
     def run(self):
         if self.distribution.has_c_libraries():
-            build_clib = self.get_finalized_command("build_clib")
-            self.include_dirs.append(os.path.join(build_clib.build_clib, "include"),)
-            self.library_dirs.insert(0, os.path.join(build_clib.build_clib, "lib64"),)
-            self.library_dirs.insert(0, os.path.join(build_clib.build_clib, "lib"),)
+            build_clib = self.get_finalized_command('build_clib')
+            self.include_dirs.append(os.path.join(build_clib.build_clib, 'include'),)
+            self.library_dirs.insert(0, os.path.join(build_clib.build_clib, 'lib64'),)
+            self.library_dirs.insert(0, os.path.join(build_clib.build_clib, 'lib'),)
 
         return _build_ext.run(self)
 
